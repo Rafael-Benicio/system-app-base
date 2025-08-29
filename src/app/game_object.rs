4afba47ded347c8ab4f14bd62d504d1bd0104c43
Rafael-Benicio <jerobenicio@gmail.com>
2025-08-29 16:43:03 -0300

    refact: ajusta função de cor e do retangulo

--- src/app/game_object.rs
+++ src/app/game_object.rs
@@ -6,36 +6,47 @@ use sdl2::rect::Rect;
 pub trait GameObject<T>: Update + Draw + Control + CollisionArea<T> {}
 
 pub trait CollisionArea<T> {
-    #[allow(dead_code)]
-    fn get_collision_body(&mut self) -> Option<&mut T> {
-        None
-    }
+        #[allow(dead_code)]
+        fn get_collision_body(&mut self) -> Option<&mut T> {
+                None
+        }
 
-    #[allow(dead_code)]
-    fn check_collision(&mut self, _entity: &mut T) {}
+        #[allow(dead_code)]
+        fn check_collision(&mut self, _entity: &mut T) {}
 
-    #[allow(dead_code)]
-    fn set_collision_status(&mut self, _state: bool) {}
+        #[allow(dead_code)]
+        fn set_collision_status(&mut self, _state: bool) {}
 }
 
 pub trait Control {
-    #[allow(dead_code)]
-    fn input(&mut self, _event_pump: &mut Event) {}
+        #[allow(dead_code)]
+        fn input(&mut self, _event_pump: &mut Event) {}
 
-    #[allow(dead_code)]
-    fn set_position(&mut self, _x: i32, _y: i32) {}
+        #[allow(dead_code)]
+        fn set_position(&mut self, _x: i32, _y: i32) {}
 }
 
 pub trait Update {
-    #[allow(dead_code)]
-    fn update(&mut self) {}
+        #[allow(dead_code)]
+        fn update(&mut self) {}
 }
 
 pub trait Draw {
-    #[allow(dead_code)]
-    fn set_color(&mut self, _r: u8, _g: u8, _b: u8) {}
+        #[allow(dead_code)]
+        fn set_color(&mut self, _r: u8, _g: u8, _b: u8) {}
 
-    fn get_color(&self) -> Color;
+        #[allow(dead_code)]
+        fn get_color(&self) -> Color {
+                Color {
+                        r: 255,
+                        g: 0,
+                        b: 0,
+                        a: 125,
+                }
+        }
 
-    fn get_draw_rect(&self) -> &Rect;
+        #[allow(dead_code)]
+        fn get_draw_rect(&self) -> Option<&Rect> {
+                None
+        }
 }
